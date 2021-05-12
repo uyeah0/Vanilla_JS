5865477bfb38b00228d41866fa5e0098e4989fbc
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-05-12 22:19:51 +0900

    #1.6~#1.10

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-alert('Im Working. Im JS. Im Beautiful. Im Worth it.')
+//alert('Im Working. Im JS. Im Beautiful. Im Worth it.');
+//const something = "something";
+
+//const daysOfWeek = ["Mon", "Tue","Wed","Thu","Fri", "Sat", "Sun", 54, true, "stuff", "lalala", something];
+
+//console.log(daysOfWeek);
+
+const yeahInfo = {
+    name: "uyeah0",
+    age: 18,
+    gender:"female",
+    isHandsome:true,
+    favMovies: ["Along the gods", "LOTR", "Oldboy"],
+    favFood:[
+        {
+            name:"pizza",
+            false:true
+        },
+        {
+            name:"Cheese burger",
+            fatty : true
+        }
+    ]
+}
+
+
+alert(yeahInfo.favFood[0].name);
