68edffca11822bdaa4ba362b7e860914e7f067dc
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-05-13 23:49:51 +0900

    #2.0~2.5
    function

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-//alert('Im Working. Im JS. Im Beautiful. Im Worth it.');
-//const something = "something";
-
-//const daysOfWeek = ["Mon", "Tue","Wed","Thu","Fri", "Sat", "Sun", 54, true, "stuff", "lalala", something];
-
-//console.log(daysOfWeek);
-
-const yeahInfo = {
-    name: "uyeah0",
-    age: 18,
-    gender:"female",
-    isHandsome:true,
-    favMovies: ["Along the gods", "LOTR", "Oldboy"],
-    favFood:[
-        {
-            name:"pizza",
-            false:true
-        },
-        {
-            name:"Cheese burger",
-            fatty : true
-        }
-    ]
+function sayHello(name, age){
+    return console.log(`'Hello ${name} you have ${age} years of age.`);
 }
 
+const greetNicolas = sayHello("uyeah",18);
+sayHello("uyeah0",18);
+//console.log('Hi!');
 
-alert(yeahInfo.favFood[0].name);
+const calculator = {
+    plus : function(a,b){
+        return a+b;
+    }
+}
+
+//
+const plus = calculator.plus(5,5);
+console.log(plus);
+
+//const title = document.getElementById("title"); 
+// Document Object Model
+console.log(title);
+
+console.error("Fuck");
+const title = document.querySelector("#title");
+
+title.innerHTML = "Hi! From Js";
+title.style.color = "red";
+document.time = "I own you now"
+
+function hadleResize(){
+    console.log("I have been");
+}
+window.addEventListener("resize",hadleResize()) // 이벤트를 받기 기다리는 것
+//handleResie호출 하는 것
+//링크 클릭할 때 유용
+
+function hadleClick(){
+    title.style.color = "red";
+}
+title.addEventListener("click", handleClick)
+
